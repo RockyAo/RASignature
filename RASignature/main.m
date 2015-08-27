640611cc25b4eaba3805219ece5a6b827da5a696
Rocky <542622608@qq.com>
2015-08-27 16:24:02 +0800

    初始化

---
+++ RASignature/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RASignature
+//
+//  Created by Rocky on 15/8/27.
+//  Copyright (c) 2015年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
